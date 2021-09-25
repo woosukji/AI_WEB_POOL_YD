30c60860cf380e9ad3a9186fd3b73bfc277075bd
somni <82272900+somnisomni@users.noreply.github.com>
2021-09-25 12:06:11 +0000

    Reinitialize repository
    회의를 통해 결정된 사안을 적용하여 리포지토리를 다시 세팅합니다.
    
    - Vue 3 대신 Vue 2를 사용합니다.
      Vue 3는 호환되지 않는 라이브러리가 아직 많아 프로젝트에
      당장 사용하기 부적합합니다. 따라서 이전 버전인 Vue 2를 사용합니다.
    
    - UI 프레임워크로 Vuetify를 사용합니다.
      Vue 3에 대응하는 Vuetify v3가 아직 alpha 단계라 사용하지 않고
      대신 ElementPlus를 사용하기로 했지만, Vue 2를 사용하기로
      결정함에 따라 UI 프레임워크 또한 Vuetify를 사용합니다.
    
    - Node.js 패키지 매니저로 npm 대신 yarn을 사용합니다.
      yarn은 npm에 비해 더 효율적이고 빠른 의존성 관리가 가능합니다.
    
    - 이 외 TypeScript, Vuex, Vue-Router 등 플러그인 사용은 유지합니다.

---
+++ Web/src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <hello-world />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import HelloWorld from "../components/HelloWorld.vue";
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  name = "Home";
+}
+</script>
