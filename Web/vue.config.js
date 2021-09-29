79cf6367bfe98e1c23a5846e04b3988112ef277d
somni <82272900+somnisomni@users.noreply.github.com>
2021-09-29 12:40:45 +0000

    Use correct `publicPath` value for GitHub Pages deployment

--- Web/vue.config.js
+++ Web/vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/AI_WEB_POOL_YD/" : "/",
   transpileDependencies: [
     "vuetify",
   ],
