b09b2a04c4d1ac3df216d22f16916559057f662b
somni <82272900+somnisomni@users.noreply.github.com>
2021-10-06 07:29:35 +0000

    Use Vuex store for login state

--- Web/src/store/index.ts
+++ Web/src/store/index.ts
@@ -1,16 +1,44 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { StoreOptions } from "vuex";
+import { IUserDisplay } from "@/interfaces/IDatabaseData";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+interface StoreState {
+  appName: string,
+  loginState: {
+    loggedIn: boolean,
+    userInfo: IUserDisplay | null,
+  }
+}
+
+const store: StoreOptions<StoreState> = {
   state: {
     appName: "POOL",
+    loginState: {
+      loggedIn: false,
+      userInfo: null,
+    },
   },
   mutations: {
+    setLoggedIn(state, data: boolean) {
+      state.loginState.loggedIn = data;
+    },
+    setLoginUserInfo(state, data: IUserDisplay) {
+      state.loginState.userInfo = data;
+    },
   },
   actions: {
+    registerLoginState({ commit }, data: IUserDisplay) {
+      commit("setLoggedIn", true);
+      commit("setLoginUserInfo", data);
+    },
+    unreigsterLoginState({ commit }) {
+      commit("setLoggedIn", false);
+      commit("setLoginUserInfo", null);
+    },
   },
-  modules: {
-  },
-});
+  modules: { },
+};
+
+export default new Vuex.Store(store);
